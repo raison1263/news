9856910b45998ff2d5fdd9e8d43197d079ba48a7
cheeze <45118382+raison1263@users.noreply.github.com>
2022-02-09 16:41:57 +0900

    Update news.js

--- news.js
+++ news.js
@@ -12,13 +12,21 @@ function call(cate)
         type:'GET',
         // https://popupu.herokuapp.com/
         url:`https://popupu.herokuapp.com/https://newsapi.org/v2/top-headlines?country=kr&apiKey=7568d0f1264d4c31a859050caecbb315${subcate}`,
-        dataType:'json',
+        dataType:'json', 
+        beforeSend: function(){
+            $('#content').append('<div class="loading">로딩중입니다.</div>');
+        },
+        complete: function()
+        {
+            $('#content .loading').remove();
+        },
+        timeout: 2000,
         success:function(getdata){
             console.log(getdata)
             usedata(getdata)
         },
         error:function(xhr){
-            console.log(xhr.status + '/' + xhr.errorText )
+            alert(xhr.status + '/' + xhr.errorText )
         }
     })
 }
